4a68c0597b1c5a1e4044f1c029055b2fea7cade4
Flame Riddle <flameriddle1@gmail.com>
2025-03-03 06:03:51 +0100

    unindex

--- script.js
+++ script.js
@@ -15,6 +15,24 @@ function win(c) {
   return 1;
 }
 
+// make numbers go around
+function loop(direction, number) {
+  switch (direction) {
+    case "decrement":
+      if (number == 0) {
+        return max - 1;
+      } else {
+        return number - 1;
+      }
+    case "increment":
+      if (number == max - 1) {
+        return 0;
+      } else {
+        return number + 1;
+      }
+  }
+}
+
 // generate on click
 function generate() {
   count = input1.value;
@@ -27,6 +45,7 @@ function generate() {
     main.innerHTML += '<a id="' + i + '"></a>';
     array.push(Math.floor(Math.random() * max));
   }
+  main.innerHTML += '<button onclick="solve()">solve!!</button>';
   // if won immediately restart
   if (win(array) == 1) {
     generate();
@@ -39,17 +58,17 @@ function generate() {
     element.addEventListener("click", () => {
       switch (id) {
         case 0:
-          array[id] = loop(array[id]);
-          array[id + 1] = loop(array[id + 1]);
+          array[id] = loop("increment", array[id]);
+          array[id + 1] = loop("increment", array[id + 1]);
           break;
         case count - 1:
-          array[id] = loop(array[id]);
-          array[id - 1] = loop(array[id - 1]);
+          array[id] = loop("increment", array[id]);
+          array[id - 1] = loop("increment", array[id - 1]);
           break;
         default:
-          array[id] = loop(array[id]);
-          array[id + 1] = loop(array[id + 1]);
-          array[id - 1] = loop(array[id - 1]);
+          array[id] = loop("increment", array[id]);
+          array[id + 1] = loop("increment", array[id + 1]);
+          array[id - 1] = loop("increment", array[id - 1]);
           break;
       }
       update();
@@ -72,11 +91,59 @@ function update() {
   }
 }
 
-// make numbers go around
-function loop(u) {
-  if (u == max - 1) {
-    return 0;
-  } else {
-    return u + 1;
+function convertToString(c) {
+  return c.toString().replace(/,/g, "");
+}
+function convertToArray(c) {
+  return c.split("").map((str) => {
+    return parseInt(str, 10);
+  });
+}
+
+function solve() {
+  const solutions = { step0: [] };
+  let temp = [];
+  for (let i = 0; i < count; i++) {
+    temp.push(0);
   }
+  solutions["step0"].push(convertToString(temp));
+
+  let i = 0;
+  do {
+    solutions["step" + (i + 1)] = [];
+    solutions["step" + i].forEach((element) => {
+      for (let j = 0; j < count; j++) {
+        temp = convertToArray(element);
+        switch (j) {
+          case 0:
+            temp[j] = loop("decrement", temp[j]);
+            temp[j + 1] = loop("decrement", temp[j + 1]);
+            break;
+          case count - 1:
+            temp[j] = loop("decrement", temp[j]);
+            temp[j - 1] = loop("decrement", temp[j - 1]);
+            break;
+          default:
+            temp[j] = loop("decrement", temp[j]);
+            temp[j + 1] = loop("decrement", temp[j + 1]);
+            temp[j - 1] = loop("decrement", temp[j - 1]);
+            break;
+        }
+        if (isElementInArray(solutions, convertToString(temp)) == false) {
+          solutions["step" + (i + 1)].push(convertToString(temp));
+        }
+      }
+    });
+    i++;
+  }while(solutions["step" + (i)].length > 0)
+  console.log(solutions);
+}
+
+function isElementInArray(dictionary, element) {
+  for (const array of Object.values(dictionary)) {
+    if (array.includes(element)) {
+      return true;
+    }
+  }
+  return false;
 }
